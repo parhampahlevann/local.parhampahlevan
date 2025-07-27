04b163eddfb4f3349c76d718d6d23786645f26eb
Parham Pahlevan <168536457+parhampahlevann@users.noreply.github.com>
2025-07-27 16:24:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,181 +1,115 @@
 #!/bin/bash
 
-# Color message functions
-function colored_msg() {
-    local color=$1
-    local message=$2
-    case $color in
-        red) echo -e "\033[31m$message\033[0m" ;;
-        green) echo -e "\033[32m$message\033[0m" ;;
-        yellow) echo -e "\033[33m$message\033[0m" ;;
-        blue) echo -e "\033[34m$message\033[0m" ;;
-        *) echo "$message" ;;
-    esac
-}
+# Configuration
+TUNNEL_IFACE="iranv6tun"
+TUNNEL_PREFIX="fdbd:1b5d:0aa8"
+PORT=8080
 
-# Check root access
-function check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        colored_msg red "This script must be run as root."
-        exit 1
-    fi
-}
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
-# Check OS compatibility
-function check_os() {
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        OS=$ID
-        VER=$VERSION_ID
-    elif type lsb_release >/dev/null 2>&1; then
-        OS=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
-        VER=$(lsb_release -sr)
-    else
-        OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-        VER=$(uname -r)
-    fi
+# Check root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${RED}This script must be run as root.${NC}"
+    exit 1
+fi
 
-    if [[ "$OS" != "ubuntu" && "$OS" != "debian" && "$OS" != "centos" && "$OS" != "fedora" ]]; then
-        colored_msg red "This script only supports Debian/Ubuntu and RHEL/CentOS/Fedora systems."
-        exit 1
-    fi
-}
-
-# Install required packages
-function install_dependencies() {
-    colored_msg blue "Installing required packages..."
-    
+# Install dependencies
+install_deps() {
+    echo -e "${BLUE}Installing dependencies...${NC}"
     if [ -f /etc/debian_version ]; then
         apt-get update
-        apt-get install -y iproute2 net-tools sed grep iputils-ping
+        apt-get install -y iproute2 net-tools socat
     elif [ -f /etc/redhat-release ]; then
-        yum install -y iproute net-tools sed grep iputils
+        yum install -y iproute net-tools socat
     fi
 }
 
-# Create IPv6 tunnel
-function create_tunnel() {
-    local location=$1
-    local iran_ipv4=$2
-    local foreign_ipv4=$3
+# Create tunnel
+create_tunnel() {
+    read -p "Enter Iran server IPv4: " IRAN_IPV4
+    read -p "Enter Foreign server IPv4: " FOREIGN_IPV4
     
-    # Common IPv6 prefix
-    local ipv6_prefix="fdbd:1b5d:0aa8"
+    echo -e "${YELLOW}Select your server location:${NC}"
+    select location in Iran Foreign; do
+        case $location in
+            Iran)
+                LOCAL_IPV6="${TUNNEL_PREFIX}::1/64"
+                REMOTE_IPV6="${TUNNEL_PREFIX}::2"
+                LOCAL_IPV4=$IRAN_IPV4
+                REMOTE_IPV4=$FOREIGN_IPV4
+                break
+                ;;
+            Foreign)
+                LOCAL_IPV6="${TUNNEL_PREFIX}::2/64"
+                REMOTE_IPV6="${TUNNEL_PREFIX}::1"
+                LOCAL_IPV4=$FOREIGN_IPV4
+                REMOTE_IPV4=$IRAN_IPV4
+                break
+                ;;
+        esac
+    done
+
+    # Create GRE tunnel over TCP (works through most restrictions)
+    echo -e "${BLUE}Creating TCP-encapsulated GRE tunnel...${NC}"
+    socat TCP-LISTEN:$PORT,fork,reuseaddr TUN:$TUNNEL_IFACE,tun-type=gre,tun-name=$TUNNEL_IFACE &
     
-    if [ "$location" == "iran" ]; then
-        local local_ipv6="${ipv6_prefix}::1/64"
-        local remote_ipv6="${ipv6_prefix}::2"
-        local local_ipv4=$iran_ipv4
-        local remote_ipv4=$foreign_ipv4
-    else
-        local local_ipv6="${ipv6_prefix}::2/64"
-        local remote_ipv6="${ipv6_prefix}::1"
-        local local_ipv4=$foreign_ipv4
-        local remote_ipv4=$iran_ipv4
-    fi
+    sleep 2
     
-    # Create tunnel interface
-    ip tunnel add ipv6tun mode sit remote $remote_ipv4 local $local_ipv4 ttl 255
-    ip link set ipv6tun up
-    ip addr add $local_ipv6 dev ipv6tun
-    ip route add ::/0 dev ipv6tun
+    # Configure tunnel
+    ip link set $TUNNEL_IFACE up
+    ip addr add $LOCAL_IPV6 dev $TUNNEL_IFACE
+    ip route add ::/0 dev $TUNNEL_IFACE
     
-    # Enable IPv6 forwarding
-    echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-    echo 1 > /proc/sys/net/ipv6/conf/default/forwarding
-    echo 1 > /proc/sys/net/ipv6/conf/ipv6tun/forwarding
+    # Enable forwarding
+    sysctl -w net.ipv6.conf.all.forwarding=1
+    sysctl -w net.ipv6.conf.default.forwarding=1
+    sysctl -w net.ipv6.conf.$TUNNEL_IFACE.forwarding=1
     
-    # Save config for uninstall
-    echo "$location $iran_ipv4 $foreign_ipv4" > /etc/ipv6tun.conf
+    # Save config
+    echo "$location $IRAN_IPV4 $FOREIGN_IPV4 $PORT" > /etc/iranv6tun.conf
     
-    colored_msg green "IPv6 tunnel created successfully!"
-    echo ""
-    colored_msg yellow "Tunnel Information:"
-    echo "Local IPv6: $local_ipv6"
-    echo "Remote IPv6: $remote_ipv6"
-    echo ""
-    colored_msg yellow "To test connection, run this command on the remote server:"
-    colored_msg blue "ping6 $remote_ipv6"
+    echo -e "${GREEN}Tunnel created successfully!${NC}"
+    echo -e "${YELLOW}Local IPv6: $LOCAL_IPV6${NC}"
+    echo -e "${YELLOW}Remote IPv6: $REMOTE_IPV6${NC}"
+    echo -e "${BLUE}On the remote server, run:${NC}"
+    echo "socat TCP:$LOCAL_IPV4:$PORT,fork,reuseaddr TUN:$TUNNEL_IFACE,tun-type=gre,tun-name=$TUNNEL_IFACE &"
 }
 
-# Remove tunnel and revert changes
-function remove_tunnel() {
-    if [ -f /etc/ipv6tun.conf ]; then
-        colored_msg blue "Removing IPv6 tunnel..."
-        
-        # Remove tunnel interface
-        ip link delete ipv6tun 2>/dev/null
-        
-        # Disable IPv6 forwarding
-        echo 0 > /proc/sys/net/ipv6/conf/all/forwarding
-        echo 0 > /proc/sys/net/ipv6/conf/default/forwarding
-        
-        # Remove config file
-        rm -f /etc/ipv6tun.conf
-        
-        colored_msg green "IPv6 tunnel removed successfully!"
-    else
-        colored_msg yellow "No IPv6 tunnel configuration found. Nothing to remove."
-    fi
+# Remove tunnel
+remove_tunnel() {
+    echo -e "${BLUE}Removing tunnel...${NC}"
+    pkill -f "socat TCP.*$TUNNEL_IFACE"
+    ip link delete $TUNNEL_IFACE 2>/dev/null
+    rm -f /etc/iranv6tun.conf
+    echo -e "${GREEN}Tunnel removed successfully!${NC}"
 }
 
 # Main menu
-function main_menu() {
-    clear
-    colored_msg blue "===================================="
-    colored_msg blue "IPv6 Tunnel between Iran and Foreign"
-    colored_msg blue "===================================="
-    echo ""
-    
-    PS3="Please select an option: "
-    options=("Create IPv6 Tunnel" "Remove IPv6 Tunnel" "Exit")
-    select opt in "${options[@]}"
-    do
-        case $opt in
-            "Create IPv6 Tunnel")
-                check_root
-                check_os
-                install_dependencies
-                
-                PS3="Select server location: "
-                locations=("Iran" "Foreign")
-                select loc in "${locations[@]}"
-                do
-                    case $loc in
-                        "Iran")
-                            location="iran"
-                            break
-                            ;;
-                        "Foreign")
-                            location="foreign"
-                            break
-                            ;;
-                        *) echo "Invalid option";;
-                    esac
-                done
-                
-                echo ""
-                colored_msg yellow "Please enter required information:"
-                read -p "Iran server IPv4: " iran_ipv4
-                read -p "Foreign server IPv4: " foreign_ipv4
-                
-                echo ""
-                colored_msg blue "Creating IPv6 tunnel..."
-                create_tunnel "$location" "$iran_ipv4" "$foreign_ipv4"
-                break
-                ;;
-            "Remove IPv6 Tunnel")
-                check_root
-                remove_tunnel
-                break
-                ;;
-            "Exit")
-                exit 0
-                ;;
-            *) echo "Invalid option";;
-        esac
-    done
-}
+echo -e "${BLUE}Iran-Foreign IPv6 Tunnel${NC}"
+echo "1. Create Tunnel"
+echo "2. Remove Tunnel"
+echo "3. Exit"
 
-# Execute main menu
-main_menu
+read -p "Select option: " choice
+
+case $choice in
+    1) 
+        install_deps
+        create_tunnel
+        ;;
+    2) 
+        remove_tunnel
+        ;;
+    3) 
+        exit 0
+        ;;
+    *) 
+        echo -e "${RED}Invalid option${NC}"
+        exit 1
+        ;;
+esac
